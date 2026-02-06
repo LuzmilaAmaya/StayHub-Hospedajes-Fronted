eb8696b8ab64fbbf5962b53551555b4e8b344dfc
luzmila amaya <amayaluzmila661@gmail.com>
2026-02-06 18:59:51 -0300

    cambios en el room

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ReservationsPage from "./pages/ReservationsPage";
+import Rooms from "./pages/Rooms";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/reservas" element={<ReservationsPage />} />
+       <Route path="/habitaciones" element={<Rooms />} />
       </Routes>
     </BrowserRouter>
   );
