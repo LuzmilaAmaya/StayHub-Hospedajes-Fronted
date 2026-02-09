b7f29a2083eda314448d1e5eff044906705ef3df
luzmila amaya <amayaluzmila661@gmail.com>
2026-02-09 20:42:17 -0300

    se creo el archivo admin y rutas para rooms

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ReservationsPage from "../pages/MyReservationsPage";
 import AdminReservationsPage from "../pages/Admin/AdminReservationsPage";
+import AdminRoomPage from "./pages/AdminRoomPage";
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
         <Route path="/" element={<HomePage />} />
         <Route path="/reservas" element={<ReservationsPage />} />
         <Route path="/adminreservas" element={<AdminReservationsPage />} />
+        <Route path="/adminhabitaciones" element={<AdminRoomPage />} />
       </Routes>
     </BrowserRouter>
   );
