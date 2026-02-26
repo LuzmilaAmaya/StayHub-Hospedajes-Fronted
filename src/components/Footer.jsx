c170a6917e2087553106f29eaf5cf1ce2c5a9d0a
Isaias Gonzalez <isaiasgonzalez3888@gmail.com>
2026-02-26 19:50:01 -0300

    Fix/Home:Se modifico la homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="py-5 border-top bg-white">
       <div className="container py-5">
         <div className="row g-5">
-          {/* LOGO + DESCRIPCION */}
+          {/* LOGO + DESCRIPCIÓN */}
           <div className="col-lg-4">
             <div className="d-flex align-items-center fw-bold mb-4">
               <div className="bg-primary-custom p-1 rounded me-2 d-flex align-items-center">
@@ -40,7 +40,7 @@ export default function Footer() {
             </ul>
           </div>
 
-          {/* COMPAÑIA */}
+          {/* COMPAÑÍA */}
           <div className="col-md-2">
             <h6 className="fw-bold mb-4">Compañía</h6>
             <ul className="list-unstyled">
@@ -76,50 +76,10 @@ export default function Footer() {
 
         {/* COPYRIGHT */}
         <div className="d-flex flex-column flex-md-row justify-content-between align-items-center mt-5 pt-4 border-top">
-          <p className="text-muted small mb-3 mb-md-0">
-            © 2024 StayHub S.A. Todos los derechos reservados.
+          <p className="text-muted small mb-0">
+            © {new Date().getFullYear()} StayHub S.A. Todos los derechos
+            reservados.
           </p>
-
-      {/* PLATAFORMA */}
-      <div className="col-md-3 offset-lg-1">
-        <h6 className="fw-bold mb-4">Plataforma</h6>
-        <ul className="list-unstyled">
-          <li className="mb-2">
-            <a href="/habitaciones" className="footer-link">Habitaciones</a>
-          </li>
-          <li className="mb-2">
-            <a href="/*" className="footer-link">Servicios VIP</a>
-          </li>
-          
-        </ul>
-      </div>
-
-      {/* COMPAÑÍA */}
-      <div className="col-md-2">
-        <h6 className="fw-bold mb-4">Compañía</h6>
-        <ul className="list-unstyled">
-          <li className="mb-2">
-            <a href="#" className="footer-link">Sobre nosotros</a>
-          </li>
-          <li className="mb-2">
-            <a href="#" className="footer-link">Contacto</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* SOCIAL */}
-      <div className="col-md-2">
-        <h6 className="fw-bold mb-4">Social</h6>
-        <div className="d-flex gap-3">
-          <a href="#" className="footer-icon">
-            <span className="material-icons">camera_alt</span>
-          </a>
-          <a href="#" className="footer-icon">
-            <span className="material-icons">facebook</span>
-          </a>
-          <a href="#" className="footer-icon">
-            <span className="material-icons">language</span>
-          </a>
         </div>
       </div>
     </footer>
